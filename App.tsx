c5a51c127fa0f00323de32c673bd0d18e94d2669
SOO <domainconnect2023@gmail.com>
2024-02-16 14:36:50 +0800

    new page

--- App.tsx
+++ App.tsx
@@ -2,18 +2,18 @@ import React, { useEffect } from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { LogBox, Platform, SafeAreaView } from 'react-native';
-import Login from './screens/LoginPage';
+import Login from './newscreens/LoginPage';
 import Register from './screens/RegisterPage';
 import { GetFCMToken, NotificationListner, requestUserPermission } from './components/pushNotification';
 import TabNavigationScreen from './screens/TabNavigation';
 import DashboardScreen from './screens/DashboardPage';
 import ProfileScreen from './screens/Profile';
 import viewImage from './screens/viewImage';
-import { firebase } from '@react-native-firebase/messaging';
 import messaging from '@react-native-firebase/messaging';
 import DeviceInfo from 'react-native-device-info';
 import { PaperProvider } from 'react-native-paper';
 import whiteTheme from './objects/commonCSS';
+import Welcome from './newscreens/Welcome';
 
 const Stack = createNativeStackNavigator();
 const isSimulator = DeviceInfo.isEmulatorSync();
@@ -46,13 +46,15 @@ function App(): JSX.Element {
     <PaperProvider theme={whiteTheme}>
       <SafeAreaView style={{ flex: 1 }}>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Login" component={Login} />
+          <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false ,navigationBarColor:"white"}}>
+            <Stack.Screen name="Welcome" component={Welcome} />
+            <Stack.Screen name="Login" component={Login}/>
+            {/* <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="Register" component={Register} />
             <Stack.Screen name="TabNavigation" component={TabNavigationScreen} />
             <Stack.Screen name="Dashboard" component={DashboardScreen} />
             <Stack.Screen name="viewImage" component={viewImage} />
-            <Stack.Screen name="Profile" component={ProfileScreen} />
+            <Stack.Screen name="Profile" component={ProfileScreen} /> */}
           </Stack.Navigator>
         </NavigationContainer>
       </SafeAreaView>
--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
   setMainContainer: {
     flex: 1,
     paddingTop: 20,
-    // backgroundColor: '#201520',
+    backgroundColor: '#ffffff',
   }
 })
 
